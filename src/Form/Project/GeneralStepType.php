2d28c8e9c3f751bafc7a04b501678b467e438185
Quentin GERGAUD <quentin.gergaud44450@gmail.com>
2026-02-09 15:41:18 +0100

    Add GeneralStepType form for project details input

---
+++ src/Form/Project/GeneralStepType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form\Project;
+
+use App\Model\ProjectData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GeneralStepType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du projet',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ProjectData::class,
+            'validation_groups' => ['general'],
+        ]);
+    }
+}
